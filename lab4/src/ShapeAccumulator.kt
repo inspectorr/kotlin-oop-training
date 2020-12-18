12c7474792b27bbcce66cca677ea39f63651e493
Roman <hmrfins@gmail.com>
2020-12-18 08:17:38 +0300

    lab4

---
+++ lab4/src/ShapeAccumulator.kt
@@ -0,0 +1,29 @@
+class ShapeAccumulator {
+    private val shapes: MutableList<Shape> = mutableListOf()
+
+    fun add(shape: Shape) {
+        shapes.add(shape)
+    }
+
+    fun addAll(shapes: Collection<Shape>) {
+        shapes.forEach { this.add(it) }
+    }
+
+    val maxAreaShape: Shape?
+        get() = shapes.maxBy { el -> el.area }
+
+    val minAreaShape: Shape?
+        get() = shapes.minBy { el -> el.area }
+
+    val maxPerimeterShape: Shape?
+        get() = shapes.maxBy { el -> el.perimeter }
+
+    val minPerimeterShape: Shape?
+        get() = shapes.minBy { el -> el.perimeter }
+
+    val totalArea: Double
+        get() = shapes.sumByDouble { el -> el.area }
+
+    val totalPerimeter: Double
+        get() = shapes.sumByDouble { el -> el.perimeter }
+}
